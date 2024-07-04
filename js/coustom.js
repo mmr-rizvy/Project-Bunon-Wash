d140ee61285c884f510c135769adc4fd8f1b6f8e
Md. Musfhikur Rahman Rizvy <rizvynews24@gmail.com>
2024-07-05 01:37:12 +0600

    First Version

---
+++ js/coustom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.customer-say-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    speed: 500,
+    dots: true
+  });
+})
